3507936d7be37d21dea1a301d8c505675de19a7c
Yash Raj <dev.yashraj52@gmail.com>
2025-08-04 16:39:23 +0530

    javascript logic to toggle skill items

--- javascript/index.js
+++ javascript/index.js
@@ -1,11 +1,26 @@
 
 
-const skillOption2 = document.querySelector("#skill-option2");
+const skillOption1 = document.querySelector("#skill-option2");
+const skillOption2 = document.querySelector("#skill-option1");
+const skillOption3 = document.querySelector("#skill-option3");
+const skillOption4 = document.querySelector("#skill-option4");
+
+
+skillOption1.onclick = function () {
+
+    skillOption1.classList.add("active");
+
+    skillOption3.classList.remove("active");
+    skillOption2.classList.remove("active");
+    skillOption4.classList.remove("active");
+
+    const skillItem = document.querySelectorAll(".skill-item");
+    skillItem.forEach(item => {
+        item.className = "skill-item framework";
+    });
+};
 
 skillOption2.onclick = function () {
-    const skillOption1 = document.querySelector("#skill-option1");
-    const skillOption3 = document.querySelector("#skill-option3");
-    const skillOption4 = document.querySelector("#skill-option4");
 
     skillOption2.classList.add("active");
 
@@ -13,4 +28,36 @@ skillOption2.onclick = function () {
     skillOption1.classList.remove("active");
     skillOption4.classList.remove("active");
 
+    const skillItem = document.querySelectorAll(".skill-item");
+    skillItem.forEach(item => {
+        item.className = "skill-item framework";
+    });
+};
+
+skillOption3.onclick = function () {
+
+    skillOption3.classList.add("active");
+
+    skillOption1.classList.remove("active");
+    skillOption2.classList.remove("active");
+    skillOption4.classList.remove("active");
+
+    const skillItem = document.querySelectorAll(".skill-item");
+    skillItem.forEach(item => {
+        item.className = "skill-item framework";
+    });
+};
+
+skillOption4.onclick = function () {
+
+    skillOption4.classList.add("active");
+
+    skillOption3.classList.remove("active");
+    skillOption1.classList.remove("active");
+    skillOption2.classList.remove("active");
+
+    const skillItem = document.querySelectorAll(".skill-item");
+    skillItem.forEach(item => {
+        item.className = "skill-item framework";
+    });
 };
