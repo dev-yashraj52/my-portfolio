22949f0710872d2fc95a1f5a84b1c4dc49cfedee
Yash Raj <dev.yashraj52@gmail.com>
2025-08-04 16:02:30 +0530

    add framework active functionality

--- javascript/index.js
+++ javascript/index.js
@@ -1,6 +1,16 @@
-function alertFunction() {
-    alert("hello");
-}
 
-const skillOption1 = document.querySelector("#skill-option2");
-skillOption1.onclick = () => alert("Hello World");
+
+const skillOption2 = document.querySelector("#skill-option2");
+
+skillOption2.onclick = function () {
+    const skillOption1 = document.querySelector("#skill-option1");
+    const skillOption3 = document.querySelector("#skill-option3");
+    const skillOption4 = document.querySelector("#skill-option4");
+
+    skillOption2.classList.add("active");
+
+    skillOption3.classList.remove("active");
+    skillOption1.classList.remove("active");
+    skillOption4.classList.remove("active");
+
+};
